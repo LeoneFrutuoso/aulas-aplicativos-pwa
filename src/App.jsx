beaf85591b7033aa59a249ffdf543562f9d7c905
FRANCISCA LEONE FRUTUOSO FERREIRA <francisca_ferreir151@estudante.sesisenai.org.br>
2026-09-09 21:55:59 -0300

    aula09.09

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,17 @@
-import { useState } from 'react'
-import heroImg from './assets/hero.png'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
 import './App.css'
-import Cabecalho from './cabecalho/cabecalho'
+import Cabecalho from './componentes/Cabecalho/Cabecalho'
+import Principal from './componentes/Principal/Principal'
+import Rodape from './componentes/Rodape/Rodape'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-<div>
-  <Cabecalho/>
-  <header>Cabeçalho</header>
-  <main>Conteudo principal</main>
-  <footer>Rodapé</footer>
-</div>
-
+    <div>
+      <Cabecalho />
+      <Principal />
+      <Rodape />
+    </div>
   )
-   
 }
+
 export default App
---
+++ src/componentes/Cabecalho/cabecalho.jsx
@@ -0,0 +1,7 @@
+import './cabecalho.css'
+
+function Cabecalho(){
+return <header className='cabecalho_root'>Cabeçalho</header>
+<img src= '/' />
+}
+export default Cabecalho
